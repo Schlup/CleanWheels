683a01d487050418518f705a11ddd98fce15ff20
Schlup <alexschlup25@gmail.com>
2024-07-10 15:59:18 -0300

    Small changes

--- client/src/pages/layout/AgendamentoBtn.jsx
+++ client/src/pages/layout/AgendamentoBtn.jsx
@@ -10,7 +10,7 @@ function AgendamentoBtn({ company }) {
     }
 
     return (
-        <button onClick={goTo()} className="active:scale-[.98] hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-indigo-700 hover:bg-indigo-950 text-white text-lg font-bold">
+        <button onClick={goTo} className="active:scale-[.98] hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-indigo-700 hover:bg-indigo-950 text-white text-lg font-bold">
             Agendar Serviço
         </button>
     )
