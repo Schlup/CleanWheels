82aa25a0f993c81a5c1c755824d697ec4206bb3c
Bernardo-Pagel <bernardo.pagel@hotmail.com>
2024-07-10 22:49:44 -0300

    style(LandingPage): text changed

--- client/src/pages/user/LandingPage.jsx
+++ client/src/pages/user/LandingPage.jsx
@@ -20,17 +20,21 @@ function LandingPage() {
           className="col-span-6 px-80 py-60 absolute bg-cover w-fit"
           style={{ backgroundImage: `url(${landingPageImage})` }}
         >
-          <div className="backdrop-opacity-10 backdrop-invert bg-white/10">
+          <div className="backdrop-opacity-25 backdrop-invert bg-white/10">
             <h1 className="p-5 text-6xl">
               NÓS <span className="text-indigo-600">GERENCIAMOS</span> SEUS
               SERVIÇOS
             </h1>
-            <h2 className="mt-12 px-5 text-3xl">Por que escolher o Clean Wheel?</h2>
+            <h2 className="mt-12 px-5 text-3xl">
+              Por que escolher o Clean Wheel?
+            </h2>
             <p className="px-5 py-1 text-xl ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-              enim necessitatibus architecto dicta asperiores obcaecati veniam
-              voluptatem exercitationem? Dolorem maxime molestias delectus,
-              officia a pariatur quis cum ipsam non id.
+              Escolher o Clean Wheel é optar pela excelência no gerenciamento de
+              serviços para empresas especializadas em cuidados automotivos. Com
+              uma plataforma robusta e intuitiva, o Clean Wheel simplifica a
+              gestão de lavação, polimento, envelopamento, revisão, manutenção,
+              funilaria e pintura, proporcionando eficiência e controle total
+              das operações.
             </p>
           </div>
           <button className="bg-indigo-700 hover:bg-indigo-950 py-2 px-12 text-white rounded-xl mt-4">
